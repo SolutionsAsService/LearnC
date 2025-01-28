c9f6fca58f6f1835f12a52242f543195f734e692
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-28 15:44:57 -0800

    Create hello_world.c

---
+++ basic_concepts/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// This is the entry point of the program
+int main() {
+    // The printf function prints text to the console
+    printf("Hello, World!\n");
+    return 0; // return 0 indicates successful completion of the program
+}
